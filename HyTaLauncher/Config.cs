21e8de2fe2f6cfb631a28c2156f48207efe7132d
MjKey <mj.dot.key@gmail.com>
2026-01-14 15:02:57 +0300

    HyTaLauncher

---
+++ HyTaLauncher/Config.cs
@@ -0,0 +1,8 @@
+namespace HyTaLauncher
+{
+    public static class Config
+    {
+        // API ключ подставляется при билде из .env файла
+        public const string CurseForgeApiKey = "$2a$10$MJpUGMXD2FTnrebdSWpYIOUeuIn95hUGTWcwf7NpxxoKFI498pZiS";
+    }
+}
